40aa005bf67b42fb6ed3c00bb76dd091b00ca9cd
Faza <150152879+faza-azka@users.noreply.github.com>
2023-12-17 16:57:07 +0700

    Add files via upload

---
+++ Job sheet 11/Contoh/Job 11 ex1.cpp
@@ -0,0 +1,32 @@
+/*
+Nama  : Faza Azka Mahasya
+NIM   : 23343038
+Prodi : Informatika
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+	char buff[255]; 
+	char text[255]; 
+	
+	FILE *fptr;
+	
+	// membuka file 
+	fptr = fopen("puisi.txt","w");
+	
+	// mengambil input dari user 
+	printf("Inputkan isi file: "); 
+	fgets(text, sizeof(text), stdin);
+	
+	// menulis ke text ke file 
+	fputs(text, fptr);
+	
+	printf("File berhasil ditulis\n");
+	
+	// tutup file 
+	fclose(fptr);
+
+}
+
